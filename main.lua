da695227dad64adaf3e3534045b8178680365d76
Florian <florian.bernier@soait.fr>
2023-02-02 15:32:43 +0100

    v2

--- main.lua
+++ main.lua
@@ -1,46 +1,29 @@
 ------VARIABLE------
+--setFullScreen : change taille ecrant true or false
+SetFullScreen = false
+
 ------TABLEAU------
-local backGroudn = {
-    x = 200,
-    y = 2,
-    y2 = -594,
-    larg = 400,
-    haut = 596,
-    speed = 40,
-    img = love.graphics.newImage("images/background.png"),
-    img2 = love.graphics.newImage("images/background2.png")
-}
-
-
 ------FONCTION LOAD------
 ------FONCTION UPDATE------
 ------FONCTION KEYPRESSED------
 ------FONCTION MOUSEPRESSED------
 ------FONCTION UTILE------
-
-
-
-
-
-
+local backGround = require("backGround")
 
 
 -----LOAD----- : ACTION DU JEU AU DEMARAGE
 function love.load()
-
+    if (SetFullScreen) then
+        love.window.setFullscreen(true)
+        screenWidth, screenHeight = love.window.getMode()
+        scale_x = screenWidth / 800
+        scale_y = screenHeight / 600
+    end
 end
 
 -----UPDATE----- : ACTION DU JEU A CHAQUE FRAME  
 function love.update(dt)
-
-    backGroudn.y = backGroudn.y + backGroudn.speed * dt
-    backGroudn.y2 = backGroudn.y2 + backGroudn.speed * dt
-    if backGroudn.y > love.graphics.getHeight() then
-        backGroudn.y = -backGroudn.haut + 2
-        backGroudn.y2 = 2
-        -- Inversez les images pour les faire alterner
-        backGroudn.img, backGroudn.img2 = backGroudn.img2, backGroudn.img
-    end
+    backGround.update(dt)
 
     -----------------------------------
     if love.mouse.isDown(1) then
@@ -51,13 +34,10 @@ end
 
 -----DRAW----- : DESSINE CE QUE TU VOIS A L'ECRAN
 function love.draw()
-    --love.graphics.setColor(1,1,1)
-    --love.graphics.rectangle("line", backGroudn.x, backGroudn.y, backGroudn.larg, backGroudn.haut)
-    love.graphics.draw(backGroudn.img, backGroudn.x, backGroudn.y, 0, backGroudn.larg / backGroudn.img:getWidth(), backGroudn.haut /
-            backGroudn.img:getHeight(), 0, 0)
-    --love.graphics.rectangle("line", backGroudn.x, backGroudn.y2, backGroudn.larg, backGroudn.haut)
-    love.graphics.draw(backGroudn.img2, backGroudn.x, backGroudn.y2, 0, backGroudn.larg / backGroudn.img2:getWidth(), backGroudn.haut /
-            backGroudn.img2:getHeight(), 0, 0)
+    --full screen scale
+    if (SetFullScreen) then love.graphics.scale(scale_x,scale_y) end
+     ----------------------------------------------------
+    backGround.draw()
 end
 
 
